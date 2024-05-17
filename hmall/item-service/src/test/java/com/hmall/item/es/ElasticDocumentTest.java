b63898e214e388d0684196304af8536f7932e9c7
Oh...Yeah!!! <614988210@qq.com>
2024-05-17 14:15:16 +0800

    批量新增文档操作

--- hmall/item-service/src/test/java/com/hmall/item/es/ElasticDocumentTest.java
+++ hmall/item-service/src/test/java/com/hmall/item/es/ElasticDocumentTest.java
@@ -2,11 +2,16 @@ package com.hmall.item.es;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmall.common.utils.CollUtils;
 import com.hmall.item.domain.po.Item;
 import com.hmall.item.domain.po.ItemDoc;
 import com.hmall.item.service.IItemService;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
@@ -21,8 +26,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.util.List;
 
-@SpringBootTest(properties = "spring.profiles.active=local")
+@SpringBootTest(properties = "spring.profiles.active=application-local")
 public class ElasticDocumentTest {
 
     private RestHighLevelClient client;
@@ -48,13 +54,66 @@ public class ElasticDocumentTest {
     }
 
     /**
+     * 批量新增数据
+     * @throws IOException
+     */
+    @Test
+    void testLoadItemDocs() throws IOException {
+        // 分页查询商品数据
+        int pageNo = 1;
+        int size = 1000;
+        while (true) {
+            Page<Item> page = itemService.lambdaQuery().eq(Item::getStatus, 1).page(new Page<Item>(pageNo, size));
+            // 非空校验
+            List<Item> items = page.getRecords();
+            if (CollUtils.isEmpty(items)) {
+                return;
+            }
+            log.info("加载第{}页数据，共{}条", pageNo, items.size());
+            // 1.创建Request
+            BulkRequest request = new BulkRequest("items");
+            // 2.准备参数，添加多个新增的Request
+            for (Item item : items) {
+                // 2.1.转换为文档类型ItemDTO
+                ItemDoc itemDoc = BeanUtil.copyProperties(item, ItemDoc.class);
+                // 2.2.创建新增文档的Request对象
+                request.add(new IndexRequest()
+                        .id(itemDoc.getId())
+                        .source(JSONUtil.toJsonStr(itemDoc), XContentType.JSON));
+            }
+            // 3.发送请求
+            client.bulk(request, RequestOptions.DEFAULT);
+
+            // 翻页
+            pageNo++;
+        }
+    }
+
+    /**
+     * 查询文档
+     * @throws IOException
+     */
+    @Test
+    void testGetDocumentById() throws IOException {
+        // 1.准备Request对象
+        GetRequest request = new GetRequest("items").id("584387");
+        // 2.发送请求
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        // 3.获取响应结果中的source
+        String json = response.getSourceAsString();
+
+        ItemDoc itemDoc = JSONUtil.toBean(json, ItemDoc.class);
+        System.out.println("itemDoc= " + itemDoc);
+    }
+
+    /**
      * 查询数据库的数据并创建索引添加
      * @throws IOException
      */
     @Test
     void testAddDocument() throws IOException {
         // 1.根据id查询商品数据
-        Item item = itemService.getById(100002644680L);
+        Item item = itemService.getById(584387);
         // 2.转换为文档类型
         ItemDoc itemDoc = BeanUtil.copyProperties(item, ItemDoc.class);
         // 3.将ItemDTO转json
