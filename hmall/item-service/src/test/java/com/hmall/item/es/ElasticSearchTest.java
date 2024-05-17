2b098aab5ea0b2d510f083ea3da17c9b3cbc65d3
Oh...Yeah!!! <614988210@qq.com>
2024-05-17 18:10:18 +0800

    根据复杂条件查询

--- hmall/item-service/src/test/java/com/hmall/item/es/ElasticSearchTest.java
+++ hmall/item-service/src/test/java/com/hmall/item/es/ElasticSearchTest.java
@@ -1,8 +1,11 @@
 package com.hmall.item.es;
 
+import cn.hutool.json.JSONUtil;
+import com.hmall.item.domain.po.ItemDoc;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -10,6 +13,8 @@ import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -45,19 +50,58 @@ public class ElasticSearchTest {
      */
     @Test
     void testMatchAll() throws IOException {
-        // 1.创建request对象
+        // 1.创建Request
+        SearchRequest request = new SearchRequest("items");
+        // 2.组织请求参数
+        request.source().query(QueryBuilders.matchAllQuery());
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+    /**
+     * 根据复杂条件查询
+     * @throws IOException
+     */
+    @Test
+    void testSearch() throws IOException {
+        // 1.创建Request
         SearchRequest request = new SearchRequest("items");
 
-        // 2.配置request参数
-        request.source()
-                .query(QueryBuilders.matchAllQuery());
+        // 2.组织请求参数
+        request.source().query(QueryBuilders.boolQuery()
+                            .must(QueryBuilders.matchQuery("name", "脱脂牛奶"))
+                            .filter(QueryBuilders.termQuery("brand", "德亚"))
+                            .filter(QueryBuilders.rangeQuery("price").lt(30000))
+        );
 
         // 3.发送请求
-        client.search(request, RequestOptions.DEFAULT);
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
 
-        System.out.println("result" + request);
+        // 4.解析响应
+        handleResponse(response);
+    }
 
 
+    /**
+     * 解析结果
+     * @param response
+     */
+    private void handleResponse(SearchResponse response) {
+        SearchHits searchHits = response.getHits();
+        // 1.获取总条数
+        long total = searchHits.getTotalHits().value;
+        System.out.println("共搜索到" + total + "条数据");
+        // 2.遍历结果数组
+        SearchHit[] hits = searchHits.getHits();
+        for (SearchHit hit : hits) {
+            // 3.得到_source，也就是原始json文档
+            String source = hit.getSourceAsString();
+            // 4.反序列化并打印
+            ItemDoc item = JSONUtil.toBean(source, ItemDoc.class);
+            System.out.println(item);
+        }
     }
 
 
